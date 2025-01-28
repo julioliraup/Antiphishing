8a9aaec4a8709d3f5a0505d923024ca423af28d8
jul10l1r4 <jul10l1r4@disroot.org>
2025-01-28 18:25:50 -0300

    :tada: Change for check and remove ghost URLs

--- update_rules.py
+++ update_rules.py
@@ -1,7 +1,8 @@
 import requests
 from re import search
-from base64 import b64encode
+from base64 import b64encode, b64decode
 from datetime import datetime
+import os
 
 phishstats_url = "https://phishstats.info/phish_score.csv"
 openphish_url = "https://openphish.com/feed.txt"
@@ -82,24 +83,94 @@ def create_suricata_rules(urls, reference, last_sid):
             if len(rule_tls) > 5: rules.append(rule_tls)
     return rules, sid
 
+def check_existing_urls():
+    valid_rules = []
+    removed_sids = []
+    
+    try:
+        with open(phishing_list, "r") as f:
+            domains = f.readlines()
+        
+        with open(output_file, "r") as f:
+            rules = f.readlines()
+        
+        for domain in domains:
+            try:
+                url = "http://" + b64decode(domain.strip()).decode()
+                response = requests.get(url, timeout=5)
+                if response.status_code == 200:
+                    continue
+            except:
+                domain_decoded = b64decode(domain.strip()).decode()
+                for rule in rules:
+                    if domain_decoded in rule:
+                        rules.remove(rule)
+                        match = search(r'sid:(\d+);', rule)
+                        if match:
+                            removed_sids.append(int(match.group(1)))
+        
+        return rules, removed_sids
+    except FileNotFoundError:
+        return [], []
+
+def create_new_ruleset(rules, available_sids):
+    sid_counter = 6000000
+    new_rules = []
+    sid_exceeded = False
+    
+    current_time = datetime.now()
+    gmt_offset = current_time.astimezone().strftime('%z')
+    formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+    
+    header = """# Suricata Antiphishing rules
+# Created by github.com/julioliraup/Antiphishing
+# Last updated: {} GMT{}
+# SID range: 6000000-6100000
+#
+""".format(formatted_time, gmt_offset)
+    
+    new_rules.append(header)
+    
+    for rule in rules:
+        if 'sid:' in rule:
+            old_sid = int(search(r'sid:(\d+);', rule).group(1))
+            if available_sids:
+                new_sid = available_sids.pop(0)
+            else:
+                new_sid = sid_counter
+                sid_counter += 1
+                
+            if new_sid > 6100000:
+                sid_exceeded = True
+            
+            new_rule = rule.replace(f'sid:{old_sid};', f'sid:{new_sid};')
+            new_rules.append(new_rule)
+    
+    return new_rules, sid_exceeded
+
 def main():
+    existing_rules, available_sids = check_existing_urls()
+    temp_output_file = output_file + ".temp"
     rules = []
-    rule = ""
-    last_sid = get_last_sid()
+    last_sid = 6000000
     
     phishstats, last_sid = create_suricata_rules(fetch_phishing_urls(phishstats_url)[8:], 'phishstats.info', last_sid)
     openphish, last_sid = create_suricata_rules(fetch_phishing_urls(openphish_url), 'openphish.com', last_sid)
     
-    rules = list(filter(None, phishstats + openphish))
+    all_rules = list(filter(None, existing_rules + phishstats + openphish))
+    new_rules, sid_exceeded = create_new_ruleset(all_rules, available_sids)
     
-    with open(output_file, "r+") as f:
-        content_file = f.read()
-        for rule in rules:
-            if rule[0:150] not in content_file:
-                f.write(rule + "\n")
+    with open(temp_output_file, "w") as f:
+        for rule in new_rules:
+            f.write(rule)
     
+    os.replace(temp_output_file, output_file)
     update_last_sid()
+    
     print(f"Rulesets: {output_file}")
+    
+    if sid_exceeded:
+        print("WARNING: SID range exceeded 6100000. Please consider adjusting the SID range.")
 
 if __name__ == "__main__":
     main()
